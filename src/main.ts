549ec1eaa4d7752e19edcb5e8fdbe6c1ed34406f
zaiying fan <zaiying.fan@realai.ai>
2023-02-02 14:22:10 +0800

    feat: 测试commit lint

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // 开始服务实例
-console.log.log('111');
+console.log.log('111222');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
