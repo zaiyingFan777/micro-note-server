1625fdb7de52919185c91630c2fdbf712bfd7421
zaiying fan <zaiying.fan@realai.ai>
2023-02-02 11:59:49 +0800

    feat: 测试commit-msg

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// 开始服务实例
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
